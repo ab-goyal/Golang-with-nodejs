4fc6d14f1b227a3270082513fb9b9afb91b4fdd2
Abhishek Goyal <abgoyal@redhat.com>
2023-02-22 18:25:33 +0530

    golang front-end and nodejs backend

---
+++ webrequests/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to my server")
+	//PerformGetRequest()
+	//PerformPostJsonRequest()
+	PerformPostFormRequest()
+}
+
+func PerformGetRequest() {
+	const myurl = "http://localhost:8000/get"
+
+	response, err := http.Get(myurl)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	fmt.Println("Status code: ", response.StatusCode)
+	fmt.Println("Content length is: ", response.ContentLength)
+
+	var responseString strings.Builder
+	content, _ := ioutil.ReadAll(response.Body)
+	byteCount, _ := responseString.Write(content)
+	fmt.Println("ByteCount is: ", byteCount)
+	fmt.Println(responseString.String())
+	//fmt.Println(content)
+	//fmt.Println(string(content))
+}
+
+func PerformPostJsonRequest() {
+	const myurl = "http://localhost:8000/post"
+
+	//fake json payload
+
+	requestBody := strings.NewReader(`
+		{
+			"Name":"Abhishek Goyal",
+			"Designation": "Software Engineer Intern",
+			"Company":"Red Hat"
+		}
+	`)
+
+	response, err := http.Post(myurl, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
+}
+
+func PerformPostFormRequest() {
+	const myurl = "http://localhost:8000/postform"
+
+	//formdata
+
+	data := url.Values{}
+	data.Add("firstname", "Abhishek")
+	data.Add("lastname", "Goyal")
+	data.Add("email", "abgoyal@redhat.com")
+
+	response, err := http.PostForm(myurl, data)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
+
+}
